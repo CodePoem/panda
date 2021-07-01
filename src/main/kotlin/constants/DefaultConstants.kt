c98738c4e14f570055f020a5c31572b2dbdd6706
CodePoem <dyb@mengbanediannao.lan>
2021-07-02 00:35:03 +0800

    :sparkles: feat(common): update README.md

--- src/main/kotlin/constants/DefaultConstants.kt
+++ src/main/kotlin/constants/DefaultConstants.kt
@@ -2,7 +2,7 @@ package constants
 
 object DefaultConstants {
     const val DEFAULT_ANDROID_HOME = "/Users/xx/Library/Android/sdk"
-    const val DEFAULT_PACKAGE_NAME = "com.mistong.kklonline.kklonline"
+    const val DEFAULT_PACKAGE_NAME = "com.vdreamers.panda"
     const val DEFAULT_EMPTY = "空空如也"
     const val DEFAULT_UNSELECT_DEVICE = "未选择设备"
 }
